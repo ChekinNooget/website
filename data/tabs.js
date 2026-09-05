438fc01ad1be51edb4db3403c0a5ddd66cd944d3
ChekinNooget <aldenyuson@gmail.com>
2026-09-05 14:14:51 -0600

    stuff

--- data/tabs.js
+++ data/tabs.js
@@ -1,10 +1,15 @@
 var pages = {
 	"Main": {},
 	"Programming Projects": {
-		"Bad Apple!!": {}
+		"Bad Apple!!": {},
 	},
 	"Video Games": {
-		"Crypt of the NecroDancer": {},
-		"FTL": {}
-	}
-}
+		"Crypt of the NecroDancer": {
+			"Review": {},
+			"Coda": {},
+			"Mods": {},
+			"Levels": {},
+		},
+		"FTL": {},
+	},
+};
